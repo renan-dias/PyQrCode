2812f3a7e1fe6bf4795da96ad1eb0381e8b524f2
Renan <renanprodias2050@gmail.com>
2022-09-07 20:51:26 -0300

    :tada: initial commit!

--- init.py
+++ init.py
@@ -2,23 +2,19 @@
 import pyqrcode
 import tkinter as tk
 
-from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 
 from PIL import Image
 
-# Functions
-window = tk.Tk()
-greeting = tk.Label(text="Hello, Tkinter")
-greeting.pack()
-
 # Variables
 link = input("Enter your link here: ")
 qr_code = pyqrcode.create(link)
 
-
-
+# Functions
+window = tk.Tk()
+greeting = tk.Label(text="Hello, Tkinter")
+greeting.pack()
 
 qr_code.png("QRCodes/QRCode.png", scale=5)
 Image.open("QRCodes/QRCode.png")
